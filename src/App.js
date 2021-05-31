82108029cc0ac870f371fb1cd510e6d18ab1aa09
Christine Yoo <christinecho.s@gmail.com>
2021-05-31 13:45:04 -0700

    created wireframe for Cart page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Account from './pages/account/Account';
+import Cart from './pages/cart/Cart';
 import Footer from './organisms/footer/Footer';
 import Landing from './pages/landing/Landing';
 import NavBar from './organisms/navbar/NavBar';
@@ -17,6 +18,7 @@ class App extends Component {
         <main>
           <Switch>
             <Route exact path='/account' component={Account} />
+            <Route exact path='/cart' component={Cart} />
             <Route exact path='/shop' component={Shop} />
             <Route exact path='/' component={Landing} />
           </Switch>
